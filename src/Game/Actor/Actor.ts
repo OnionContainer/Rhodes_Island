a37dc7f2074ee686ef3ac915c8a645f1df502cda
OnionContainer <zhangtsongda@gmail.com>
2020-03-17 20:50:54 -0400

    微调
    删掉了Enemy和Oprt，以后干员和敌人都统一用Actor表示

--- src/Game/Actor/Actor.ts
+++ src/Game/Actor/Actor.ts
@@ -8,40 +8,9 @@ import { ActorType } from "../../Common/DodKey";
 import ActorStateMgr, { ActorStateID } from "./State/ActorStateFsm";
 import { Blocker } from "./ActorModules/Blocker";
 
-/**
- * 这个东西不是enum是个历史遗留问题
- * 凑合用吧，Actor.Identity里可以拿到
- * TO 阿葱： 懒死你……
- */
 
- //tmp remove
-// /**
-//  * 作者：葱
-//  * 这里只是记录一下Actor需要有什么接口，然后丢给Actor继承一下防报错
-//  * 此类本身没有实际意义
-//  * 请勿使用
-//  * 
-//  * 另：这些操作似乎违反了单一职责原则
-//  * Actor应该只开放“操作接口”
-//  * 而Actor.profile只开放“数据访问接口”
-//  * 待讨论
-//  */
-// abstract class ActorRequire{
 
-//     setAbleToBlock(state:boolean):void{}//设为可以进行阻挡/不可以进行阻挡
 
-//     ableToBlock():boolean{return true;}//可以进行阻挡
-
-//     setMoveable(state:boolean):void{}//设为可以移动/不可以移动
-
-//     moveable():boolean{return true;}//可以进行移动
-
-//     setBlocked(state:boolean):void{}//设为已被阻挡/未被阻挡
-
-//     isBlocked():boolean{return true;}//已被阻挡
-
-//     ableToBeBlocked():boolean{return true;}//可以被阻挡
-// }
 
 
 //基本原则：少用继承，多用组合
